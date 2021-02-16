c9abe0407807a67c9b47cd9772e26fef630b63ba
David Betancur <davo.beta.fer@gmail.com>
2021-02-15 22:03:22 -0800

    add 10 possitive quotes

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ const randomNum = num => {
 };
 
 const messages = {
-    positiveQuotes: ['possitive quote 1', 'possitive quote 2', 'possitive quote 3', 'possitive quote 4', 'possitive quote 5',
-                'possitive quote 6', 'possitive quote 7', 'possitive quote 8', 'possitive quote 9', 'possitive quote 10'],
+    positiveQuotes: ['What is now proved was once only imagined', 'Turn your obstacles into opportunities and your problems into possibilities', 
+                'There is no good and evil, there is only power and those too weak to seek it', 'No act of kindness, no matter how small, is ever wasted', 
+                'Stay strong, stay positive, and never give up','Never lose hope. Storms make people stronger and never last forever', 'Anything is possible. Anything can be', 
+                'Nothing can disturb your peace of mind unless you allow it to', 'I\'m not going to limit myself just because people won\'t accept the fact that I can do something else.', 
+                'With our thoughts we make the world'],
+                //Roy T Bennett, J.K.Rowling, William Blake, Shel Silverstein, Dolly Parton, Buddha
     powerTasks: ['power task 1', 'power task 2', 'power task 3', 'power task 4', 'power task 5', 
                 'power task 6', 'power task 7', 'power task 8', 'power task 9', 'power task 10'],
     affirmations: ['affirmation 1', 'affirmation 2', 'affirmation 3', 'affirmation 4', 'affirmation 5', 
